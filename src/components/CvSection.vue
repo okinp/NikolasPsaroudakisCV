25f5a9844c77fe0952f609465d6da506d92675d5
Nikolas Psaroudakis <nikolas.psaroudakis@rokoko.com>
2022-03-15 11:43:58 +0200

    Added Rokoko

--- src/components/CvSection.vue
+++ src/components/CvSection.vue
@@ -1,55 +1,56 @@
 <template>
-  <section class="content">
-    <h4>{{ sectionData.label }}</h4>
-    <ul>
-      <li
-        v-for="(entry, eIdx) in sectionData.entries"
-        :key="eIdx"
-        :style="getPageBreaks(entry)"
-      >
-        <p class="date">{{ entry.date }}</p>
-        <div class="detail">
-          <div class="detail__title" v-if="entry.title">{{ entry.title }}</div>
-          <div v-if="entry.organization">{{ entry.organization }}</div>
-          <span v-if="entry.extra">{{ entry.extra }}</span>
-          <div class="tags" v-if="entry.tags">
-            <span
-              class="tag"
-              v-for="(tag, tIdx) in entry.tags.sort()"
-              :key="tIdx"
+    <section
+        class="content"
+        :style="{ 'page-break-before': sectionData.label === 'Skills Matrix' ? 'always' : 'none' }"
+    >
+        <h4>{{ sectionData.label }}</h4>
+        <ul>
+            <li
+                v-for="(entry, eIdx) in sectionData.entries"
+                :key="eIdx"
+                :style="getPageBreaks(entry)"
             >
-              {{ tag }}
-            </span>
-          </div>
-        </div>
-      </li>
-    </ul>
-  </section>
+                <p class="date">{{ entry.date }}</p>
+                <div class="detail">
+                    <div class="detail__title" v-if="entry.title">{{ entry.title }}</div>
+                    <div v-if="entry.organization">{{ entry.organization }}</div>
+                    <span v-if="entry.extra">{{ entry.extra }}</span>
+                    <div class="tags" v-if="entry.tags">
+                        <span
+                            class="tag"
+                            v-for="(tag, tIdx) in entry.tags.sort()"
+                            :key="tIdx"
+                        >{{ tag }}</span>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </section>
 </template>
 <script>
 export default {
-  name: "cv-section",
-  props: {
-    sectionData: {
-      type: Object,
-      default: () => ({})
-    }
-  },
-  methods: {
-    getPageBreaks({ id, type }) {
-      const ID = type + "-" + id;
-      let style = {
-        "page-break-before": "auto",
-        "page-break-after": "auto",
-        "page-break-inside": "avoid"
-      };
+    name: "cv-section",
+    props: {
+        sectionData: {
+            type: Object,
+            default: () => ({})
+        }
+    },
+    methods: {
+        getPageBreaks({ id, type }) {
+            const ID = type + "-" + id;
+            let style = {
+                "page-break-before": "auto",
+                "page-break-after": "auto",
+                "break-inside": "avoid"
+            };
 
-      if (ID === "work-1") {
-        style = { ...style, "page-break-before": "always" };
-      }
-      return style;
+            if (ID === "work-5" || ID === "work-12") {
+                style = { ...style, "page-break-before": "always" };
+            }
+            return style;
+        }
     }
-  }
 };
 </script>
 <style lang="scss" scoped>
@@ -57,69 +58,69 @@ $color: #4d5357;
 $color-dark: darken($color, 10%);
 $color-light: lighten($color, 10%);
 section.content {
-  display: flex;
-  flex-direction: column;
-  margin-top: 10px;
-  h4 {
-    text-transform: uppercase;
-    font-size: 0.85em;
-    color: $color;
-    margin-top: 1rem;
-    margin-bottom: 1.8em;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-    margin-top: 0.2em;
-    margin-bottom: 0px;
-    li {
-      display: flex;
-      align-items: flex-start;
-      justify-content: flex-start;
-      font-size: 0.8em;
-      margin-bottom: 10px;
-      margin-top: 14px;
-      page-break-before: auto;
-      page-break-inside: avoid;
-      p.date {
-        margin: 0;
-        text-align: right;
-        min-width: 22%;
-        max-width: 22%;
-      }
-      .detail {
-        margin-left: 1em;
-        min-width: 78%;
-        max-width: 78%;
-        display: flex;
-        flex-direction: column;
-        > .detail__title {
-          // font-weight: semi-bold;
-        }
-        > span {
-          margin-top: 5px;
-          color: $color;
-          font-size: 0.95em;
-        }
-      }
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+    h4 {
+        text-transform: uppercase;
+        font-size: 0.85em;
+        color: $color;
+        margin-top: 1rem;
+        margin-bottom: 1.8em;
     }
-  }
+    ul {
+        list-style-type: none;
+        padding: 0;
+        margin-top: 0.2em;
+        margin-bottom: 0px;
+        li {
+            display: flex;
+            align-items: flex-start;
+            justify-content: flex-start;
+            font-size: 0.8em;
+            margin-bottom: 10px;
+            margin-top: 14px;
+            page-break-before: auto;
+            page-break-inside: avoid;
+            p.date {
+                margin: 0;
+                text-align: right;
+                min-width: 22%;
+                max-width: 22%;
+            }
+            .detail {
+                margin-left: 1em;
+                min-width: 78%;
+                max-width: 78%;
+                display: flex;
+                flex-direction: column;
+                > .detail__title {
+                    // font-weight: semi-bold;
+                }
+                > span {
+                    margin-top: 5px;
+                    color: $color;
+                    font-size: 0.95em;
+                }
+            }
+        }
+    }
 }
 .tags {
-  display: flex;
-  flex-wrap: wrap;
-  margin: 7px 0 0 -3px;
-  span.tag {
-    box-sizing: border-box;
-    padding: 1px 2px;
-    margin: 3px;
-    border-radius: 3px;
-    color: $color-dark;
-    background-color: lighten($color-light, 50%);
-    border: 1px solid lighten($color-light, 42%);
-    border-radius: 3px;
-    font-size: 10px;
-    text-transform: uppercase;
-  }
+    display: flex;
+    flex-wrap: wrap;
+    margin: 7px 0 0 -3px;
+    span.tag {
+        box-sizing: border-box;
+        padding: 1px 2px;
+        margin: 3px;
+        border-radius: 3px;
+        color: $color-dark;
+        background-color: lighten($color-light, 50%);
+        border: 1px solid lighten($color-light, 42%);
+        border-radius: 3px;
+        font-size: 10px;
+        text-transform: uppercase;
+    }
 }
 </style>
--- src/components/CvSkillsMatrix.vue
+++ src/components/CvSkillsMatrix.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="skills-matrix">
-    <h4>{{ dataProp.label }}</h4>
-    <div class="skills-matrix__grid">
-      <cv-skill-score  v-for="(entry,idx) in dataProp.entries" :key="idx" :subject="entry.label" :score="entry.score"/>
+    <div class="skills-matrix" :style="{ 'page-break-before': 'always' }">
+        <h4>{{ dataProp.label }}</h4>
+        <div class="skills-matrix__grid">
+            <cv-skill-score
+                v-for="(entry, idx) in dataProp.entries"
+                :key="idx"
+                :subject="entry.label"
+                :score="entry.score"
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import CvSkillScore from "./CvSkillScore";
 export default {
-  name: "cv-skills-matrix",
-  components:{
-    CvSkillScore
-  },
-  props: {
-    dataProp: {
-      type: Object,
-      default: () => ({
-        label: "Skills Matrix",
-        entries: [ ]
-      })
+    name: "cv-skills-matrix",
+    components: {
+        CvSkillScore
+    },
+    props: {
+        dataProp: {
+            type: Object,
+            default: () => ({
+                label: "Skills Matrix",
+                entries: []
+            })
+        }
     }
-  }
 }
 </script>
 <style lang="scss" scoped>
@@ -30,26 +35,26 @@ $color: #4d5357;
 $color-dark: darken($color, 10%);
 $color-light: lighten($color, 10%);
 .skills-matrix {
-  display: flex;
-  flex-direction: column;
-  h4 {
-    text-transform: uppercase;
-    font-size: 0.85em;
-    color: $color;
-    margin-top: 0;
-    margin-bottom: 0.8em;
-  }
+    display: flex;
+    flex-direction: column;
+    h4 {
+        text-transform: uppercase;
+        font-size: 0.85em;
+        color: $color;
+        margin-top: 0;
+        margin-bottom: 0.8em;
+    }
 }
 .skills-matrix__grid {
-  display: grid;
-  margin: 0 auto;
-  align-self: center;
-  min-width: 78%;
-  max-width: 78%;
-  width: 100%;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 4px;
-  margin-top: 0.4em;
-  margin-bottom: 1em;
+    display: grid;
+    margin: 0 auto;
+    align-self: center;
+    min-width: 78%;
+    max-width: 78%;
+    width: 100%;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 4px;
+    margin-top: 0.4em;
+    margin-bottom: 1em;
 }
 </style>
